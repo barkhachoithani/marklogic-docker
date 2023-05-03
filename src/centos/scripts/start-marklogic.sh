4c41ed3172f25344406e727c7beb63103d8952ed
Barkha Choithani <barkha.choithani@marklogic.com>
2023-05-03 13:55:28 -0700

    added retry to check bootstrap host

--- src/centos/scripts/start-marklogic.sh
+++ src/centos/scripts/start-marklogic.sh
@@ -284,9 +284,10 @@ fi
 if [[ -f /var/opt/MarkLogic/DOCKER_JOIN_CLUSTER ]]; then
     info "MARKLOGIC_JOIN_CLUSTER is true, but skipping join because this instance has already joined a cluster."
 elif [[ "${MARKLOGIC_JOIN_CLUSTER}" == "true" ]]; then
-    HOST_RESP_CODE=$(curl --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" -m 30 -s -o bootstraphost.json -w "%{http_code}" -X GET http://"${MARKLOGIC_BOOTSTRAP_HOST}":8002/manage/v2/hosts/"${MARKLOGIC_BOOTSTRAP_HOST}"?format=json)
-    if [[ ${HOST_RESP_CODE} -eq 200 ]]; then
-        BOOTSTRAP_HOST_ID=$(cat bootstraphost.json | jq '."host-default".id')
+    HOST_RESP_CODE=$(curl --retry 5 --retry-all-errors --retry-max-time 60 http://"${MARKLOGIC_BOOTSTRAP_HOST}":8002/manage/v2/hosts/"${MARKLOGIC_BOOTSTRAP_HOST}"?format=json --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" \
+                -o bootstraphost.json -X GET -H "Accept: application/json")
+    if [[ "${HOST_RESP_CODE}" -eq 200 ]]; then
+        BOOTSTRAP_HOST_ID=$(grep -o '"id": "[^"]*' bootstraphost.json | grep -o '[^"]*$')
         LOCAL_HOST_ID=$(curl --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" -m 30 -s -X GET http://localhost:8002/manage/v2/hosts/"${HOSTNAME}"?format=json | jq '."host-default".id')
         if [[ "${BOOTSTRAP_HOST_ID}" == "${LOCAL_HOST_ID}" ]]; then
             info "HOST cannot join itself, skipped joining cluster."
