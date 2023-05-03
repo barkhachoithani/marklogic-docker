e11f65f57426beffb55da03d1313f28305ebb625
Barkha Choithani <barkha.choithani@marklogic.com>
2023-05-03 16:17:38 -0700

    curl option fixes

--- src/centos/scripts/start-marklogic.sh
+++ src/centos/scripts/start-marklogic.sh
@@ -285,7 +285,7 @@ if [[ -f /var/opt/MarkLogic/DOCKER_JOIN_CLUSTER ]]; then
     info "MARKLOGIC_JOIN_CLUSTER is true, but skipping join because this instance has already joined a cluster."
 elif [[ "${MARKLOGIC_JOIN_CLUSTER}" == "true" ]]; then
     HOST_RESP_CODE=$(curl --retry 6 --retry-max-time 60 http://"${MARKLOGIC_BOOTSTRAP_HOST}":8002/manage/v2/hosts/"${MARKLOGIC_BOOTSTRAP_HOST}"?format=json --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" \
-                -o bootstraphost.json -X GET -H "Accept: application/json")
+                -w "%{http_code}" -o bootstraphost.json -X GET -H "Accept: application/json")
     if [[ "${HOST_RESP_CODE}" -eq 200 ]]; then
         BOOTSTRAP_HOST_ID=$(grep -o '"id": "[^"]*' bootstraphost.json | grep -o '[^"]*$')
         LOCAL_HOST_ID=$(curl --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" -m 30 -s -X GET http://localhost:8002/manage/v2/hosts/"${HOSTNAME}"?format=json | jq '."host-default".id')
