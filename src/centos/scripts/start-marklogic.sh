566bec0d206046c81cb7d4ce14d891a3f5fab7ce
Barkha Choithani <barkha.choithani@marklogic.com>
2023-05-04 09:50:41 -0700

    lint fixes

--- src/centos/scripts/start-marklogic.sh
+++ src/centos/scripts/start-marklogic.sh
@@ -165,7 +165,7 @@ function curl_retry_validate {
             retry_count=$((retry_count + 1))
             if [[ $retry_count -ge $N_RETRY ]]; then
                 if [[ "${return_response_code}" = "true" ]]; then
-                    return ${response_code}
+                    return "${response_code}"
                 else
                     error "Expected response code ${2}, got ${response_code} from ${1}." exit
                 fi
@@ -174,7 +174,7 @@ function curl_retry_validate {
         fi
     done
     
-    return ${response_code}
+    return "${response_code}"
 }
 
 ################################################################
@@ -294,7 +294,7 @@ fi
 if [[ -f /var/opt/MarkLogic/DOCKER_JOIN_CLUSTER ]]; then
     info "MARKLOGIC_JOIN_CLUSTER is true, but skipping join because this instance has already joined a cluster."
 elif [[ "${MARKLOGIC_JOIN_CLUSTER}" == "true" ]]; then
-    HOST_RESP_CODE=$(curl_retry_validate "http://"${MARKLOGIC_BOOTSTRAP_HOST}":8002/manage/v2/hosts/"${MARKLOGIC_BOOTSTRAP_HOST}"?format=json" 200 "-o bootstraphost.json -X GET -H \"Accept: application/json\" --anyauth --user \"${ML_ADMIN_USERNAME}\":\"${ML_ADMIN_PASSWORD}\"" "true")
+    HOST_RESP_CODE=$(curl_retry_validate "http://"${MARKLOGIC_BOOTSTRAP_HOST}":8002/manage/v2/hosts/"${MARKLOGIC_BOOTSTRAP_HOST}"?format=json" 200 "-o bootstraphost.json -X GET -H \"Accept: application/json\" --anyauth --user \"${ML_ADMIN_USERNAME}\":\"${ML_ADMIN_PASSWORD}\"" true)
     if [[ "${HOST_RESP_CODE}" -eq 200 ]]; then
         BOOTSTRAP_HOST_ID=$(grep -o '"id": "[^"]*' bootstraphost.json | grep -o '[^"]*$')
         LOCAL_HOST_ID=$(curl --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" -m 30 -s -X GET http://localhost:8002/manage/v2/hosts/"${HOSTNAME}"?format=json | jq '."host-default".id')
