98d42d4b2e17d06f85a429983976befd08378b34
Barkha Choithani <barkha.choithani@marklogic.com>
2023-05-16 05:08:14 -0700

    added tests to verify incorrect bootstrap host

--- src/centos/scripts/start-marklogic.sh
+++ src/centos/scripts/start-marklogic.sh
@@ -300,7 +300,7 @@ elif [[ "${MARKLOGIC_JOIN_CLUSTER}" == "true" ]]; then
     curl_retry_validate "http://${MARKLOGIC_BOOTSTRAP_HOST}:8002/manage/v2/hosts?format=json" 200 "-o bootstraphost.json -X GET -H \"Accept: application/json\" --anyauth --user \"${ML_ADMIN_USERNAME}\":\"${ML_ADMIN_PASSWORD}\"" true
     HOST_RESP_CODE=$?
     if [[ "${HOST_RESP_CODE}" -eq 200 ]]; then
-        BOOTSTRAP_HOST_ID=$(jq -r '.["host-default-list"]["list-items"]["list-item"][] | select(.roleref == "bootstrap") | .idref' bootstraphost.json)
+        BOOTSTRAP_HOST_ID=$(jq '.["host-default-list"]["list-items"]["list-item"][] | select(.roleref == "bootstrap") | .idref' bootstraphost.json)
         LOCAL_HOST_ID=$(curl --anyauth --user "${ML_ADMIN_USERNAME}":"${ML_ADMIN_PASSWORD}" -m 30 -s -X GET http://localhost:8002/manage/v2/hosts/"${HOSTNAME}"?format=json | jq '."host-default".id')
         if [[ "${BOOTSTRAP_HOST_ID}" == "${LOCAL_HOST_ID}" ]]; then
             info "HOST cannot join itself, skipped joining cluster."
